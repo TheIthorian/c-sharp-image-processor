2c7627c29dbbe639ca2603d2e1f8843c48eb26da
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-01 21:54:23 +0100

    fix: fix invert filter

--- ImageProcessor/src/InvertFilter.cs
+++ ImageProcessor/src/InvertFilter.cs
@@ -7,9 +7,9 @@ class InvertFilter : IFilter
 
     public void Process(Bitmap buffer)
     {
-        for (int y = 0; y < buffer.Height; y++)
+        for (int y = 1; y < buffer.Height; y++)
         {
-            for (int x = 0; x < buffer.Width; x++)
+            for (int x = 1; x < buffer.Width; x++)
             {
                 var color = buffer.GetPixel(x, y);
                 buffer.SetPixel(x, y, Color.FromArgb(color.A, 255 - color.R, 255 - color.G, 255 - color.B));
