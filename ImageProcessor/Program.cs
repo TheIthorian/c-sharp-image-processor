a427b6ce6c6ed953f1cd17f65c49c0461acaad8a
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-01 20:08:35 +0100

    fix: add support tag for Program

--- ImageProcessor/Program.cs
+++ ImageProcessor/Program.cs
@@ -1,19 +1,24 @@
-﻿Console.WriteLine("Processing image...");
-
-foreach (string arg in args)
+﻿[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+class Program
 {
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Processing image...");
+
+        foreach (string arg in args) { }
+
+        var imageReader = new ImageReader("../image.jpg");
+
+        var invertFilter = FilterFactory.FromString("InvertFilter");
+        var invertFilterNode = new FilterNode(invertFilter);
+        invertFilterNode.ConnectInput(imageReader);
+
+        var writer = new ImageWriter("../output.jpg");
+        writer.ConnectInput(invertFilterNode);
+
+        writer.Write();
+
+        Console.WriteLine("Done!");
+    }
 }
-// ExampleProcessor.Run();
-
-var imageReader = new ImageReader("../image.jpg");
-
-var invertFilter = FilterFactory.FromString("InvertFilter");
-var invertFilterNode = new FilterNode(invertFilter);
-invertFilterNode.ConnectInput(imageReader);
-
-var writer = new ImageWriter("../output.jpg");
-writer.ConnectInput(invertFilterNode);
-
-writer.Write();
-
-Console.WriteLine("Done!");
