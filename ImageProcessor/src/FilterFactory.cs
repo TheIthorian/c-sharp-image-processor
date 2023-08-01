455b2f559f5b6e98ec764970f8682bb928deb1bf
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-01 21:54:40 +0100

    feat: implement filter factory methods

--- ImageProcessor/src/FilterFactory.cs
+++ ImageProcessor/src/FilterFactory.cs
@@ -1,14 +1,14 @@
 [System.Runtime.Versioning.SupportedOSPlatform("windows")]
 class FilterFactory
 {
-    public enum FILTER
+    public enum Filter
     {
-        INVERT,
-        MIRROR,
-        SOBEL,
-        GRAY_SCALE,
-        BLACK_AND_WHITE,
-        FOURIER
+        Invert,
+        Mirror,
+        Sobel,
+        Gray_Scale,
+        Black_And_White,
+        Fourier
     }
 
     public static IFilter FromArray(Array filterArray)
@@ -17,8 +17,19 @@ class FilterFactory
         return new InvertFilter();
     }
 
+    public static IFilter From(Filter filter)
+    {
+        return filter switch
+        {
+            Filter.Invert => new InvertFilter(),
+            Filter.Mirror => new MirrorFilter(),
+            _ => throw new Exception("Unsupported filter: " + filter.ToString()),
+        };
+    }
+
     public static IFilter FromString(string filterName)
     {
-        return new InvertFilter();
+        Filter filter = (Filter)Enum.Parse(typeof(Filter), filterName);
+        return From(filter);
     }
 }
