87f57c428ac3d7190d76c696c3d35267a7ee9cb2
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-02 21:50:07 +0100

    test: use test images

--- ImageProcessorTests/InvertFilterTest.cs
+++ ImageProcessorTests/InvertFilterTest.cs
@@ -10,15 +10,14 @@ public class InvertFilterTest
     [TestMethod]
     public void TestInvertsImageColors()
     {
-        var testImage = TestUtil.MakeImage(50, 50);
-        TestUtil.Fill(testImage, 0, 100, 200);
+        var testImage = TestUtil.LoadStandardImage();
 
         var invertFilter = FilterFactory.From(FilterFactory.Filter.Invert);
         invertFilter.Process(testImage);
 
         TestUtil.AssertEqual(
             testImage,
-            TestUtil.LoadImage("InvertFilterTest.TestInvertsImageColorsResult")
+            TestUtil.LoadImage("InvertFilterTest.TestInvertsImageColors")
         );
     }
 }
