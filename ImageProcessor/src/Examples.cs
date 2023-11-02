4da07bf25ba4fa5db01156762859cae5a9eaf18a
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-11-02 18:30:28 +0000

    refactor: move examples to own class

---
+++ ImageProcessor/src/Examples.cs
@@ -0,0 +1,59 @@
+using ImageProcessorNS;
+
+[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+class Examples
+{
+    private static readonly FileLogger logger = new();
+
+    public static void RunPerformanceTests()
+    {
+        var tester = new PerformanceTester(logger);
+        tester.Test();
+    }
+
+    public static void ProcessFiles(string[] args)
+    {
+        string inputFilePath = GetInputFilePathFromArgs(args);
+        string outputFilePath = GetOutputFilePathFromArgs(args);
+
+        logger.WriteLine($"\nReading image from {inputFilePath}");
+        logger.WriteLine("Processing image...");
+
+        var imageReader = new ImageReader(inputFilePath);
+
+        var statsNode = FilterFactory.Stats(imageReader);
+        var outputNode = FilterFactory.EdgeDetection(statsNode);
+        var writer = new ImageWriter(outputFilePath);
+
+        writer.ConnectInput(outputNode);
+
+        var elapsedMilliseconds = writer.Write();
+
+        logger.WriteLine("Done! Image processed in " + elapsedMilliseconds + "ms.");
+        logger.WriteLine("Output image saved to " + outputFilePath + '\n');
+    }
+
+    private static string GetInputFilePathFromArgs(string[] args)
+    {
+        return args.Length == 0 ? FindFirstImageInCurrentDirectory() : args[0];
+    }
+
+    private static string FindFirstImageInCurrentDirectory()
+    {
+        var files = Directory.GetFiles(Directory.GetCurrentDirectory());
+        var inputFilePath = files.FirstOrDefault(file => file.EndsWith(".jpg") || file.EndsWith(".png"));
+
+        if (inputFilePath == null)
+        {
+            throw new AppExceptions.NoInputConnected("Unable to find input image in current directory.");
+        }
+
+        return inputFilePath;
+    }
+
+    private static string GetOutputFilePathFromArgs(string[] args)
+    {
+        return args.Length < 2 ? Path.Combine(Directory.GetCurrentDirectory(), "output.jpg") : args[1];
+    }
+
+}
